cca0dc9375a6482a3909fb3c48920aea47f2ba14
Recomagnesius <buzhor.danyl@gmail.com>
2022-10-29 20:02:28 -0500

    fix: fix js refactoring problems

--- app.js
+++ app.js
@@ -33,17 +33,17 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='any-item-block-input';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="any-item-block-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit any-item-block-button";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete any-item-block-button";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
